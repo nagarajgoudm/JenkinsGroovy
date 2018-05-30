2a1ed5e25d5f7d5cdea6bd30168cfb628319c1c5
nagaraj <m.nagaraj@simbalabs.com>
2018-05-30 19:58:36 +0530

    Generated with JHipster 4.12.0

---
+++ src/main/java/com/sql/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.sql.repository.search;
+
+import com.sql.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
